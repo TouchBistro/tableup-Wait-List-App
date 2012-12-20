24be5a03f3cf343d19d5127288b79d8d2741c946
Padraic Doyle <padraic@givefit.com>
2012-12-20 09:35:30 -0500

    potential fix for header issue

--- WaitList/CGAddGuestViewController.m
+++ WaitList/CGAddGuestViewController.m
@@ -449,30 +449,35 @@
 }
 
 
-//- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section{
-//    if (section == 0){
-//        UIView* customView = [[UIView alloc] initWithFrame:CGRectMake(0,0,300,30)];
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section{
+    if (section == 0){
+        UIView *customView = [[UIView alloc] initWithFrame:CGRectMake(0,0,320,30)];
         
-//        NSString *label = @"Parties: ";
-//        label = [label stringByAppendingString:self.totalParties.stringValue];
-//        label = [label stringByAppendingString:@" / Guests: "];
-//        label = [label stringByAppendingString:self.totalGuests.stringValue];
-//        label = [label stringByAppendingString:@" / Estimated Wait: "];
-//        label = [label stringByAppendingString:self.estimatedWait.stringValue];
-//        label = [label stringByAppendingString:@" mins"];
+        UILabel *headerLabel = [[UILabel alloc] initWithFrame:customView.frame];
+        headerLabel.backgroundColor = [UIColor clearColor];
+        headerLabel.font = [UIFont systemFontOfSize:10];
+        headerLabel.frame = CGRectMake(0,0,300,30);
         
-//        UILabel *headerLabel = [[UILabel alloc] initWithFrame:CGRectZero];
-//        headerLabel.backgroundColor = [UIColor clearColor];
-//        headerLabel.font = [UIFont systemFontOfSize:10];
-//        headerLabel.frame = CGRectMake(0,0,300,30);
-//        headerLabel.text =  label;
-//        headerLabel.textAlignment = UITextAlignmentCenter;
-//        headerLabel.textColor = [UIColor redColor];
+        headerLabel.text =  @"Parties: ";
+        headerLabel.text = [headerLabel.text stringByAppendingString:self.totalParties.stringValue];
+        headerLabel.text = [headerLabel.text stringByAppendingString:@" / Guests: "];
+        headerLabel.text = [headerLabel.text stringByAppendingString:self.totalGuests.stringValue];
+        headerLabel.text = [headerLabel.text stringByAppendingString:@" / Estimated Wait: "];
+        headerLabel.text = [headerLabel.text stringByAppendingString:self.estimatedWait.stringValue];
+        headerLabel.text = [headerLabel.text stringByAppendingString:@" mins"];
         
-//        [customView addSubview:headerLabel];
-//        return customView;
-//    }
-//}
+        
+        headerLabel.textAlignment = UITextAlignmentCenter;
+        headerLabel.textColor = [UIColor redColor];
+        
+        [customView addSubview:headerLabel];
+        headerLabel = nil;
+        
+        return customView;
+    }else{
+        return nil;
+    }
+}
 
 - (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
 {
